16b5a94c27ec64b38fa9f4ad74c688b7c20b18d8
Ogunshola1 <68999986+Ogunshola1@users.noreply.github.com>
2020-07-30 15:45:32 +0100

    This is my first cimmit

---
+++ DemoProject123/PageObject/RegistrationPage.cs
@@ -0,0 +1,86 @@
+﻿using DemoProject123.Utilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoProject123.PageObject
+{
+    class RegistrationPage
+    {
+        public RegistrationPage()
+        {
+            driver = Hooks1.driver;
+        }
+
+        IWebDriver driver;
+        
+
+        IWebElement helloSignIn => driver.FindElement(By.XPath("//*[@id='nav-link-accountList']/div/span"));
+        
+        IWebElement CreateYourAmazonAccount1 => driver.FindElement(By.Id("createAccountSubmit"));
+
+        IWebElement yourName => driver.FindElement(By.Id("ap_customer_name"));
+
+        IWebElement email => driver.FindElement(By.CssSelector("#ap_email"));
+
+        IWebElement password => driver.FindElement(By.CssSelector("#ap_password"));
+
+        IWebElement reEnterpassword => driver.FindElement(By.CssSelector("#ap_password_check"));
+
+        IWebElement CreateYourAmazonAccount2 => driver.FindElement(By.CssSelector("#continue"));
+
+
+        public void ClickOnHelloSignIn()
+        {
+            helloSignIn.Click();
+        }
+
+        public void ClickOnCreateYourAmazonAccount1()
+        {
+            CreateYourAmazonAccount1.Click();
+        }
+
+        public void EnterYourName()
+        {
+            yourName.SendKeys("Folake");
+        }
+
+        public void EnterEmail()
+        {
+            email.SendKeys("folake.richardson@talktalk.net");
+        }
+
+        public void EnterPassword()
+        {
+            password.SendKeys("PasswordSecure");
+        }
+
+        public void EnterPassword2()
+        {
+            reEnterpassword.SendKeys("PasswordSecure");
+        }
+
+        public void ClickOnCreateYourAmazonAccount2()
+        {
+            CreateYourAmazonAccount2.Click();
+        }
+
+        public void NavigateTowebsite()
+        {
+            driver.Navigate().GoToUrl("https://www.amazon.co.uk");
+        }
+
+        
+
+
+
+
+
+        
+
+
+
+    }
+}
---
+++ DemoProject123/StepDefinition/RegistrationSteps.cs
@@ -0,0 +1,101 @@
+﻿using DemoProject123.PageObject;
+using System;
+using TechTalk.SpecFlow;
+
+namespace DemoProject123.StepDefinition
+{
+    [Binding]
+    public class RegistrationSteps
+    {
+        RegistrationPage registrationPage;
+
+        public RegistrationSteps()
+        {
+            registrationPage = new RegistrationPage();
+        }
+        
+        
+        
+        [Given(@"I navigate to amazon website")]
+        public void GivenINavigateToAmazonWebsite()
+        {
+            registrationPage.NavigateTowebsite();
+        }
+
+        [When(@"I click on Hello sign in")]
+        public void WhenIClickOnHelloSignIn()
+        {
+            registrationPage.ClickOnHelloSignIn();
+        }
+
+        [When(@"I click on create amazon account")]
+        public void WhenIClickOnCreateAmazonAccount()
+        {
+            registrationPage.ClickOnCreateYourAmazonAccount1();
+        }
+
+        [When(@"I enter my name")]
+        public void WhenIEnterMyName()
+        {
+            registrationPage.EnterYourName();
+        }
+
+        [When(@"I enter my email")]
+        public void WhenIEnterMyEmail()
+        {
+            registrationPage.EnterEmail();
+        }
+
+        [When(@"I enter my password")]
+        public void WhenIEnterMyPassword()
+        {
+            registrationPage.EnterPassword();
+        }
+
+        [When(@"I re enter my password")]
+        public void WhenIReEnterMyPassword()
+        {
+            registrationPage.EnterPassword2();
+        }
+
+        [When(@"I create your amazon accounbt on the registration page")]
+        public void WhenICreateYourAmazonAccountOnTheRegistrationPage()
+        {
+            registrationPage.ClickOnCreateYourAmazonAccount2();
+        }
+
+        [Then(@"my account should be created")]
+        public void ThenMyAccountShouldBeCreated()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+
+        [When(@"I created another step for demo purpose")]
+        public void WhenICreatedAnotherStepForDemoPurpose()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        }
+
+    }
+
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
